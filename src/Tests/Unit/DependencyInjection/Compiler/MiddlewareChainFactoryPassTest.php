fcb0004ca6091035708834c12eefd8f1c9c5f8d5
Matej Bádal <matejbadal@gmail.com>
2020-09-02 17:47:56 +0200

    added tests

--- src/Tests/Unit/DependencyInjection/Compiler/MiddlewareChainFactoryPassTest.php
+++ src/Tests/Unit/DependencyInjection/Compiler/MiddlewareChainFactoryPassTest.php
@@ -463,4 +463,357 @@ class MiddlewareChainFactoryPassTest extends TestCase
         $compilerPass->process($container);
         $this->assertTrue(true);
     }
+
+    public function testCanDetectNonExistingMiddlewareChainDuringRoutingRulesCompilation()
+    {
+        /** @var MockInterface|DeepCopy $deepCopy */
+        $deepCopy = Mockery::mock(DeepCopy::class);
+
+        $compilerPass = new MiddlewareChainFactoryPass(
+            $deepCopy
+        );
+
+        /** @var MockInterface|ContainerBuilder $container */
+        $container = Mockery::mock(ContainerBuilder::class);
+        $container
+            ->shouldReceive('hasParameter')
+            ->once()
+            ->withArgs(
+                [
+                    'psr15'
+                ]
+            )
+            ->andReturn(true);
+
+        $middlewareAliases = [
+            '1',
+            '2',
+            '3'
+        ];
+        $container
+            ->shouldReceive('getParameter')
+            ->once()
+            ->withArgs(
+                [
+                    'psr15'
+                ]
+            )
+            ->andReturn(
+                [
+                    'use_cache'         => false,
+                    'middleware_chains' => [
+                        'Test' => $middlewareAliases
+                    ],
+                    'routing'           => [
+                        'Condition' => [
+                            'middleware_chain' => 'ABCD'
+                        ]
+                    ]
+                ]
+            );
+
+        /** @var MockInterface|Definition $definition */
+        $definition = Mockery::mock(Definition::class);
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    SymfonyControllerAdapter::class
+                ]
+            )
+            ->andReturn(
+                $definition
+            );
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    RouteNameStrategyResolver::class
+                ]
+            )
+            ->andReturn(
+                null
+            );
+
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    CompiledPathStrategyResolver::class
+                ]
+            )
+            ->andReturn(
+                null
+            );
+
+        $newDefinitionArray = [];
+        foreach ($middlewareAliases as $alias) {
+            $container
+                ->shouldReceive('hasDefinition')
+                ->once()
+                ->withArgs(
+                    [
+                        $alias
+                    ]
+                )
+                ->andReturn(true);
+
+            /** @var MockInterface|Definition $definition */
+            $definition = Mockery::mock(Definition::class);
+            $container
+                ->shouldReceive('getDefinition')
+                ->once()
+                ->withArgs(
+                    [
+                        $alias
+                    ]
+                )
+                ->andReturn(
+                    $definition
+                );
+
+            /** @var MockInterface|Definition $definition */
+            $newDefinition = Mockery::mock(Definition::class);
+            $newDefinition
+                ->shouldReceive('setPublic')
+                ->once()
+                ->withArgs(
+                    [
+                        false
+                    ]
+                )->andReturn(
+                    $newDefinition
+                );
+
+            $newDefinition
+                ->shouldReceive('setShared')
+                ->once()
+                ->withArgs(
+                    [
+                        false
+                    ]
+                );
+
+            $newDefinitionArray[] = $newDefinition;
+            $deepCopy
+                ->shouldReceive('copy')
+                ->once()
+                ->withArgs(
+                    [
+                        $definition
+                    ]
+                )
+                ->andReturn(
+                    $newDefinition
+                );
+        }
+
+        $newDefinitionArray[0]
+            ->shouldReceive('addMethodCall')
+            ->once()
+            ->withArgs(
+                [
+                    'append',
+                    [
+                        $newDefinitionArray[1],
+                    ]
+                ]
+            );
+
+        $newDefinitionArray[0]
+            ->shouldReceive('addMethodCall')
+            ->once()
+            ->withArgs(
+                [
+                    'append',
+                    [
+                        $newDefinitionArray[2],
+                    ]
+                ]
+            );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Error in condition config: [Condition]. Middleware chain with name: [ABCD] does not exist');
+        $compilerPass->process($container);
+    }
+
+    public function testCanDetectEmptyConditionsOnRoutingConfig()
+    {
+        /** @var MockInterface|DeepCopy $deepCopy */
+        $deepCopy = Mockery::mock(DeepCopy::class);
+
+        $compilerPass = new MiddlewareChainFactoryPass(
+            $deepCopy
+        );
+
+        /** @var MockInterface|ContainerBuilder $container */
+        $container = Mockery::mock(ContainerBuilder::class);
+        $container
+            ->shouldReceive('hasParameter')
+            ->once()
+            ->withArgs(
+                [
+                    'psr15'
+                ]
+            )
+            ->andReturn(true);
+
+        $middlewareAliases = [
+            '1',
+            '2',
+            '3'
+        ];
+        $container
+            ->shouldReceive('getParameter')
+            ->once()
+            ->withArgs(
+                [
+                    'psr15'
+                ]
+            )
+            ->andReturn(
+                [
+                    'use_cache'         => false,
+                    'middleware_chains' => [
+                        'Test' => $middlewareAliases
+                    ],
+                    'routing'           => [
+                        'Condition' => [
+                            'middleware_chain' => 'Test',
+                            'conditions'       => [],
+                        ]
+                    ]
+                ]
+            );
+
+        /** @var MockInterface|Definition $definition */
+        $definition = Mockery::mock(Definition::class);
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    SymfonyControllerAdapter::class
+                ]
+            )
+            ->andReturn(
+                $definition
+            );
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    RouteNameStrategyResolver::class
+                ]
+            )
+            ->andReturn(
+                null
+            );
+
+        $container
+            ->shouldReceive('getDefinition')
+            ->once()
+            ->withArgs(
+                [
+                    CompiledPathStrategyResolver::class
+                ]
+            )
+            ->andReturn(
+                null
+            );
+
+        $newDefinitionArray = [];
+        foreach ($middlewareAliases as $alias) {
+            $container
+                ->shouldReceive('hasDefinition')
+                ->once()
+                ->withArgs(
+                    [
+                        $alias
+                    ]
+                )
+                ->andReturn(true);
+
+            /** @var MockInterface|Definition $definition */
+            $definition = Mockery::mock(Definition::class);
+            $container
+                ->shouldReceive('getDefinition')
+                ->once()
+                ->withArgs(
+                    [
+                        $alias
+                    ]
+                )
+                ->andReturn(
+                    $definition
+                );
+
+            /** @var MockInterface|Definition $definition */
+            $newDefinition = Mockery::mock(Definition::class);
+            $newDefinition
+                ->shouldReceive('setPublic')
+                ->once()
+                ->withArgs(
+                    [
+                        false
+                    ]
+                )->andReturn(
+                    $newDefinition
+                );
+
+            $newDefinition
+                ->shouldReceive('setShared')
+                ->once()
+                ->withArgs(
+                    [
+                        false
+                    ]
+                );
+
+            $newDefinitionArray[] = $newDefinition;
+            $deepCopy
+                ->shouldReceive('copy')
+                ->once()
+                ->withArgs(
+                    [
+                        $definition
+                    ]
+                )
+                ->andReturn(
+                    $newDefinition
+                );
+        }
+
+        $newDefinitionArray[0]
+            ->shouldReceive('addMethodCall')
+            ->once()
+            ->withArgs(
+                [
+                    'append',
+                    [
+                        $newDefinitionArray[1],
+                    ]
+                ]
+            );
+
+        $newDefinitionArray[0]
+            ->shouldReceive('addMethodCall')
+            ->once()
+            ->withArgs(
+                [
+                    'append',
+                    [
+                        $newDefinitionArray[2],
+                    ]
+                ]
+            );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Error in condition config: [Condition]. At least one condition has to be specified');
+        $compilerPass->process($container);
+    }
 }
